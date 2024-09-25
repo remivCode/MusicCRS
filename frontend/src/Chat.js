c51245ba906ef6d3c2d88c4f3364c65a96301329
remivCode <remi.vialleton@gmail.com>
2024-09-25 13:53:44 +0200

    fix chat component

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,6 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function Chat() {
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement("script");
+
+    // Set the script attributes
+    script.type = "text/javascript";
+    script.src =
+      "https://cdn.jsdelivr.net/npm/iaigroup-chatwidget@latest/build/bundle.min.js";
+
+    // Append the script to the document body (or head)
+    document.body.appendChild(script);
+  }, []);
   return (
     <div
       id="chatWidgetContainer"
@@ -8,7 +21,6 @@ function Chat() {
       data-server-url="http://127.0.0.1:5000"
       data-use-feedback
       data-use-login
-      data-socketio-path="/socket.io"
     ></div>
   );
 }
