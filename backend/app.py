ff24278c7de3bf70eb34a90fa8911b3c5999682e
remivCode <remi.vialleton@gmail.com>
2024-09-26 17:15:47 +0200

    adding basic test communication using socketio library

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
@@ -6,14 +6,18 @@ app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])  # Autorise l'origine localhost:3000
 socketio = SocketIO(app, cors_allowed_origins=["http://localhost:3000"])
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@socketio.on('connect')
+def handle_connect():
+    print(f"Client connected: {request.sid}")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print(f"Client disconnected: {request.sid}")
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f"Message reçu : {msg}")
-    socketio.send(msg)  # renvoie le message à tous les clients
+    print(f"Message reçu : {msg['message']}")
+    socketio.emit('message', {"message": {"text": "test"}}, room=request.sid)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
